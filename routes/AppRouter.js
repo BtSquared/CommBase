feecbd58555ba8bd220a88de57030a1ee1c7d8ac
Tanner Mitchell <TannerTMitchell@gmail.com>
2021-10-11 12:47:45 -0500

    added auth0

--- routes/AppRouter.js
+++ routes/AppRouter.js
@@ -2,4 +2,9 @@ const { Router } = require('express')
 
 const AppRouter = Router()
 
+// app.get('/', (req, res) => {
+//   console.log(req.oidc.isAuthenticated())
+//   res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out')
+// })
+
 module.exports = AppRouter

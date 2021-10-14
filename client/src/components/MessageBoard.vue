36691f9d8c3c52c59a8ee6151d0b85d2786ff432
Tanner Mitchell <TannerTMitchell@gmail.com>
2021-10-13 21:41:36 -0500

    end of day commit

--- client/src/components/MessageBoard.vue
+++ client/src/components/MessageBoard.vue
@@ -9,6 +9,8 @@
 </template>
 
 <script>
+import axios from 'axios'
+import {BASE_URL} from '../globals'
 import PostForm from '../subComponents/PostForm.vue'
 
 export default {
@@ -17,6 +19,7 @@ export default {
     PostForm
   },
   prop: {
+    serverId: String,
     channelName: String,
     channelId: String,
     posts: Array
@@ -28,8 +31,14 @@ export default {
     handleFormChange(value) {
       this.NewPostContent = value
     },
-    handleFormSubmit() {
-
+    async handleFormSubmit() {
+      const res = await axios.post(`${BASE_URL}post/createpost`, {
+        serverId: this.serverId,
+        channelId:  this.channelId,
+        userId: this.userId,
+        content: this.NewPostContent
+      })
+      this.NewPostContent = ''
     }
   }
 }

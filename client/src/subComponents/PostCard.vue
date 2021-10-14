89ff094986d5c927eebbfe289b38e5ef6ccac593
Tanner Mitchell <TannerTMitchell@gmail.com>
2021-10-14 10:05:59 -0500

    added PostCard basic set up

---
+++ client/src/subComponents/PostCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div>
+      <h2>{{userName}}</h2>
+      <p>{{content}}</p>
+    </div>
+    <div>
+      <button>Edit</button>
+      <button>Delete</button>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "postCard",
+  props: {
+    userName: String,
+    content: String
+  }
+}
+</script>

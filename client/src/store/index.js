3bc8035080396491053525735c5b163dd814454c
Tanner Mitchell <TannerTMitchell@gmail.com>
2021-10-15 17:27:09 -0500

    come on auth0 lets go check out whats behind the shed yeah just look that way 🔫 *click clack bang*

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,13 +1,24 @@
-import Vue from 'vue'
 import Vuex from 'vuex'
-
+import Vue from 'vue'
+import actions from './auth/actions'
+import getters from './auth/getters'
+import mutations from './auth/mutations'
+import auth0 from 'auth0-js'
 Vue.use(Vuex)
-
-const store = new Vuex.Store({
-  state: {
-    user: {}
-  },
-  mutations: {
-    setUser(state) {}
-  }
+const state = () => ({
+  userIsAuthorized: false,
+  auth0: new auth0.WebAuth({
+    domain: process.env.VUE_APP_AUTH0_CONFIG_DOMAIN,
+    clientID: process.env.VUE_APP_AUTH0_CONFIG_CLIENTID,
+    redirectUri: process.env.VUE_APP_DOMAINURL_REDIRECT,
+    responseType: process.env.VUE_APP_AUTH0_CONFIG_RESPONSETYPE,
+    scope: process.env.VUE_APP_AUTH0_CONFIG_SCOPE
+  })
+})
+export default new Vuex.Store({
+  namespaced: true,
+  state,
+  actions,
+  getters,
+  mutations
 })
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,28 +1,17 @@
 <template>
   <div class="home">
-    <ServerList v-if="disabled" :servers="servers" />
-  <div v-if="disabled">
-    <form @submit.prevent="handleSubmit">
-      <input 
-        @input="handleChange" 
-        type="file"
-        name="avatar"
-        placeholder="avatar"
-      />
-      <button type="Submit">Submit</button>
-    </form>
-  </div>
-  <div>
-    <!-- Check that the SDK client is not currently loading before accessing is methods -->
-    <div v-if="!$auth.loading">
-      <!-- show login when not authenticated -->
-      <button v-if="!$auth.isAuthenticated" @click="login">Log in</button>
-      <!-- show logout when authenticated -->
-      <button v-if="$auth.isAuthenticated" @click="logout">Log out</button>
+      <ServerList v-if="disabled" :servers="servers" />
+    <div v-if="disabled">
+      <form @submit.prevent="handleSubmit">
+        <input 
+          @input="handleChange" 
+          type="file"
+          name="avatar"
+          placeholder="avatar"
+        />
+        <button type="Submit">Submit</button>
+      </form>
     </div>
-    <!-- NEW - add a route to the profile page -->
-      <router-link v-if="$auth.isAuthenticated" to="/profile">Profile</router-link>
-  </div>
   </div>
 </template>
 
@@ -66,16 +55,6 @@ export default {
       const res = await axios.post(`${BASE_URL}/user/avatar`, formData)
       console.log(res)
     },
-    // Log the user in
-    login() {
-      this.$auth.loginWithRedirect()
-    },
-    // Log the user out
-    logout() {
-      this.$auth.logout({
-        returnTo: window.location.origin
-      })
-    }
   }
 }
 </script>

46a5419c2c4a43507c736e40e2b119e94012b8ae
Tanner Mitchell <TannerTMitchell@gmail.com>
2021-10-18 10:18:51 -0500

    changed build to dist

--- app.js
+++ app.js
@@ -20,9 +20,9 @@ app.use('/api', AppRouter)
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')))
+  app.use(express.static(path.join(__dirname, 'client/dist')))
   app.get('*', (req, res) => {
-    res.sendFile(path.join(`${__dirname}/client/build/index.html`))
+    res.sendFile(path.join(`${__dirname}/client/dist/index.html`))
   })
 }
 
